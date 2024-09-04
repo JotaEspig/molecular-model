d352fa4a24440b7cfdc637546ec7f193f1af5489
João Vitor Espig <jotaespig@gmail.com>
2024-09-04 12:22:42 -0300

    using new method

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include "app.hpp"
 
 int main() {
-  App app{true};
-  app.set_title("Bencke cria");
-  app.set_height(600);
-  app.set_width(600);
-  app.main_loop();
-  return 0;
+    App app{true};
+    app.set_title("Bencke cria");
+    app.set_window_size(600, 600);
+    app.main_loop();
+    return 0;
 }
