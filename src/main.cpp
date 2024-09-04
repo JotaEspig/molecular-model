5209e413ebfb21661b48c5ee8857848d355a5f79
João Vitor Espig <jotaespig@gmail.com>
2024-09-04 12:08:54 -0300

    removed binary executable

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,10 @@
-#include <iostream>
-
-#include "atom.hpp"
 #include "app.hpp"
 
 int main() {
-    App app{true};
-    app.set_title("Bencke cria");
-    app.set_height(600);
-    app.set_width(600);
-    app.main_loop();
-    return 0;
+  App app{true};
+  app.set_title("Bencke cria");
+  app.set_height(600);
+  app.set_width(600);
+  app.main_loop();
+  return 0;
 }
